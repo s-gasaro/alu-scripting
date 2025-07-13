0cd4b828ad216661bc0d91cd7b09854997fb2e6b
Sarah Gasaro <s.gasaro1@alustudent.com>
2025-07-13 11:11:59 -0700

    YSL

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-"""Function that queries the Reddit API and prints the titles of the first 10 hot posts."""
+"""prints the titleof the first top 10 hot posts listed for a given subreddit"""
 import requests
 
+
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts of a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "python:subreddit.top_ten:v1.0 (by u/yourusername)"}
-    params = {"limit": 10}
+    """main function"""
+    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url, headers=headers)
 
-    try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
-        if response.status_code != 200:
-            print(None)
-            return
-
-        data = response.json().get("data", {}).get("children", [])
-        if not data:
-            print(None)
-            return
-
-        for post in data:
-            print(post["data"]["title"])
-
-    except Exception:
+    if response.status_code == 200:
+        data = response.json()['data']
+        for post in data['children'][:10]:
+            print(post['data']['title'])
+    else:
         print(None)
